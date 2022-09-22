0874de2f14ff9cb185fb645eba43fdac556ec81e
MaxBrandtner <110912574+MaxBrandtner@users.noreply.github.com>
2022-09-22 22:45:36 +0700

    fixed #58

--- bash-scripts/hooks_generator.sh
+++ bash-scripts/hooks_generator.sh
@@ -10,9 +10,9 @@ sed -i 's/_INSERT_VM_NAME_/$1/' .tmp/hooks/release/end/revert.sh
 
 mkdir -p /etc/libvirt/hooks/qemu.d/$1
 
-ls /etc/libvirt/hooks/qemu.d/$1/hooks
+ls /etc/libvirt/hooks/qemu.d/$1
 hooks_exists=$?
 if [ hooks_exists != 0 ]
 then
-	cp -R .tmp/hooks /etc/libvirt/hooks/qemu.d/$1/hooks
+	cp -R .tmp/hooks /etc/libvirt/hooks/qemu.d/$1/
 fi
